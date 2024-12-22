781d702d2e6d5fc909d2683e2eb35ea64985fd5b
muhammed <mmkocak6565@gmail.com>
2024-12-22 21:47:36 +0300

    galeri izni eklendi

--- app/src/main/java/com/example/artbookjava/ArtActivity.java
+++ app/src/main/java/com/example/artbookjava/ArtActivity.java
@@ -1,15 +1,20 @@
 package com.example.artbookjava;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.view.View;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 import com.example.artbookjava.databinding.ActivityArtBinding;
+import com.google.android.material.snackbar.Snackbar;
 
 public class ArtActivity extends AppCompatActivity {
     private ActivityArtBinding binding;
@@ -20,7 +25,7 @@ public class ArtActivity extends AppCompatActivity {
         binding = ActivityArtBinding.inflate(getLayoutInflater());
         View view = binding.getRoot();
         setContentView(view);
-        
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.nameText), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
@@ -31,6 +36,20 @@ public class ArtActivity extends AppCompatActivity {
 
     }
     public void selectImage(View view){
+        if(ContextCompat.checkSelfPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED){
+            if (ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.READ_EXTERNAL_STORAGE)){
+                Snackbar.make(view, "Permission needed for gallery", Snackbar.LENGTH_INDEFINITE).setAction("Give Permission", new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
 
+                    }
+                }).show();
+            }else {
+                //request permission
+            }
+
+        }else{
+            // gallery
+        }
     }
 }
