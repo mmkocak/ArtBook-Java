bd201cb52ff854045e4cd8fbbeca9118cc711b49
muhammed <mmkocak6565@gmail.com>
2024-12-23 18:29:05 +0300

    SQLite veritabanı oluşturuldu ve art fonksiyonu yazıldı

--- app/src/main/java/com/example/artbookjava/MainActivity.java
+++ app/src/main/java/com/example/artbookjava/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.artbookjava;
 
 import android.content.Intent;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
@@ -16,8 +18,11 @@ import androidx.core.view.WindowInsetsCompat;
 
 import com.example.artbookjava.databinding.ActivityMainBinding;
 
+import java.util.ArrayList;
+
 public class MainActivity extends AppCompatActivity {
     private ActivityMainBinding binding;
+    ArrayList<Art> artArrayList;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -31,6 +36,26 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+        artArrayList = new ArrayList<>();
+        getData();
+
+    }
+    private  void  getData(){
+    try {
+        SQLiteDatabase sqLiteDatabase = this.openOrCreateDatabase("Arts", MODE_PRIVATE, null);
+        Cursor cursor = sqLiteDatabase.rawQuery("SELECT * FROM  arts", null);
+        int nameIx = cursor.getColumnIndex("artname");
+        int idIx = cursor.getColumnIndex("id");
+        while (cursor.moveToNext()){
+            String name = cursor.getString(nameIx);
+            int id = cursor.getInt(idIx);
+            Art art = new Art(name, id);
+            artArrayList.add(art);
+        }
+        cursor.close();
+    } catch (Exception e) {
+
+    }
     }
 
     @Override
