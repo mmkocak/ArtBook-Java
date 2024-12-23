7cca010d847cc191caa21b4feabefc884263f443
muhammed <mmkocak6565@gmail.com>
2024-12-23 16:58:15 +0300

    küçük görseller oluşturmak için fonksiyonlar oluşturuldu

--- app/src/main/java/com/example/artbookjava/ArtActivity.java
+++ app/src/main/java/com/example/artbookjava/ArtActivity.java
@@ -27,6 +27,9 @@ import androidx.core.view.WindowInsetsCompat;
 import com.example.artbookjava.databinding.ActivityArtBinding;
 import com.google.android.material.snackbar.Snackbar;
 
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+
 public class ArtActivity extends AppCompatActivity {
     private ActivityArtBinding binding;
     ActivityResultLauncher<Intent> activityResultLauncher;
@@ -48,7 +51,29 @@ public class ArtActivity extends AppCompatActivity {
         registerLauncher();
     }
     public void save(View view){
+    String name = binding.nameText.getText().toString();
+    String artistName = binding.artistText.getText().toString();
+    String year = binding.yearText.getText().toString();
 
+    Bitmap smallImage = makeSmallerImage(selectedImage, 300);
+        ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+        smallImage.compress(Bitmap.CompressFormat.PNG, 50, outputStream);
+        byte[] byteArray=  outputStream.toByteArray();
+    }
+    public Bitmap makeSmallerImage(Bitmap image, int maximumSize){
+        int width = image.getWidth();
+        int height = image.getHeight();
+        float bitmapRatio = (float) width / (float) height;
+        if(bitmapRatio > 1){
+            //landscape image
+            width = maximumSize;
+            height = (int)(width * bitmapRatio);
+        }else {
+            //portrait image
+            height = maximumSize;
+            width = (int) (height * bitmapRatio);
+        }
+        return image.createScaledBitmap(image, width, height, true);
     }
     public void selectImage(View view){
         if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU){
