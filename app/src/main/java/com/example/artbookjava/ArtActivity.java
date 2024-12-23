519eaf9bbf9ef18e7ee4162ee387a8f63a04110a
muhammed <mmkocak6565@gmail.com>
2024-12-23 15:27:16 +0300

    Launcher oluşturuldu

--- app/src/main/java/com/example/artbookjava/ArtActivity.java
+++ app/src/main/java/com/example/artbookjava/ArtActivity.java
@@ -1,11 +1,22 @@
 package com.example.artbookjava;
 
 import android.Manifest;
+import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.graphics.ImageDecoder;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.view.View;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
+import androidx.activity.result.ActivityResult;
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
@@ -18,6 +29,9 @@ import com.google.android.material.snackbar.Snackbar;
 
 public class ArtActivity extends AppCompatActivity {
     private ActivityArtBinding binding;
+    ActivityResultLauncher<Intent> activityResultLauncher;
+    ActivityResultLauncher<String> permissionLauncher;
+    Bitmap selectedImage;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -50,6 +64,45 @@ public class ArtActivity extends AppCompatActivity {
 
         }else{
             // gallery
+            Intent intentToGallery = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
         }
+    }
+    private  void registerLauncher(){
+        activityResultLauncher = registerForActivityResult(new ActivityResultContracts.StartActivityForResult(), new ActivityResultCallback<ActivityResult>() {
+            @Override
+            public void onActivityResult(ActivityResult o) {
+                if(o.getResultCode() == RESULT_OK){
+                   Intent intentFromResult  = o.getData();
+                   if(intentFromResult != null){
+                       Uri imageData = intentFromResult.getData();
+                       //binding.imageView.setImageURI(imageData);
+                       try{
+                           if(Build.VERSION.SDK_INT >= 28){
+                               ImageDecoder.Source source = ImageDecoder.createSource(getContentResolver(), imageData);
+                               selectedImage = ImageDecoder.decodeBitmap(source);
+                               binding.imageView.setImageBitmap(selectedImage);
+                           }else{
+                               selectedImage = MediaStore.Images.Media.getBitmap(ArtActivity.this.getContentResolver(), imageData);
+                               binding.imageView.setImageBitmap(selectedImage);
+                           }
+
+                       }catch (Exception e){
+                           e.printStackTrace();
+                       }
+                   }
+                }
+            }
+        });
+        permissionLauncher = registerForActivityResult(new ActivityResultContracts.RequestPermission(), new ActivityResultCallback<Boolean>() {
+            @Override
+            public void onActivityResult(Boolean o) {
+                if(o){
+                    // permission granted
+                }else{
+                    // permission denied
+                    Toast.makeText(ArtActivity.this, "Permission needed!", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
     }
 }
